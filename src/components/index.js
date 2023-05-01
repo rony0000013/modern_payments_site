5134b0a98ea7744ba1cd6e608596c79d09e388d0
Rounak Sen <103501472+rony0000013@users.noreply.github.com>
2023-05-01 23:06:59 +0530

    init

---
+++ src/components/index.js
@@ -0,0 +1,32 @@
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedbackCard from "./FeedbackCard";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+import GetStarted from "./GetStarted";
+
+export {
+    Navbar,
+    Stats,
+    Testimonials,
+    Hero,
+    GetStarted,
+    FeedbackCard,
+    Footer,
+    CTA,
+    CardDeal,
+    Clients,
+    Billing,
+    Business,
+    Button
+};
+
+
+
